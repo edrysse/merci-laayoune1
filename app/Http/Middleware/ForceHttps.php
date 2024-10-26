78743a43dd404fd02603f47625c3839d21f9c57a
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-26 08:17:22 +0100

    last commit

---
+++ app/Http/Middleware/ForceHttps.php
@@ -0,0 +1,15 @@
+namespace App\Http\Middleware;
+
+use Closure;
+
+class ForceHttps
+{
+    public function handle($request, Closure $next)
+    {
+        if (!$request->isSecure()) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
--- resources/views/auth/includes/header.blade.php
+++ resources/views/auth/includes/header.blade.php
@@ -3,6 +3,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>auth page</title>
+
+
     <!--===============================================================================================-->
     <link rel="icon" type="image/png" href="{{ asset('loginpage/images/icons/favicon.ico') }}" />
     <!--===============================================================================================-->
