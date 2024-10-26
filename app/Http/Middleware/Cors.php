590acfb2f1f3d1fa0673525c1e35bb01daa317b0
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-26 08:10:44 +0100

    new file cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,17 @@
+// app/Http/Middleware/Cors.php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*') // أو النطاقات التي ترغب في السماح بها
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    }
+}
