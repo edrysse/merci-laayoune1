8803d6ef8a957e6503919684b41368d4e8926fd9
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-27 16:40:24 +0100

    last

--- resources/views/auth/includes/header.blade.php
+++ resources/views/auth/includes/header.blade.php
@@ -1,24 +1,21 @@
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>auth page</title>
 
-
-    <!--===============================================================================================-->
+    <!-- Icon -->
     <link rel="icon" type="image/png" href="{{ asset('loginpage/images/icons/favicon.ico') }}" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/vendor/bootstrap/css/bootstrap.min.css') }}" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}" />
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/vendor/animate/animate.css') }}">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/vendor/select2/select2.min.css') }}">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/css/util.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('loginpage/css/main.css') }}">
-    <!--===============================================================================================-->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('loginpage/vendor/bootstrap/css/bootstrap.min.css') }}">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="{{ asset('loginpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+
+    <!-- Additional CSS -->
+    <link rel="stylesheet" href="{{ asset('loginpage/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" href="{{ asset('loginpage/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('loginpage/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('loginpage/css/util.css') }}">
+    <link rel="stylesheet" href="{{ asset('loginpage/css/main.css') }}">
 </head>
