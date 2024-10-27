0b689fcd8eef4b6edd9c3f21f9c2d0f18e6396c4
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-27 17:36:40 +0100

    last commit

--- resources/views/client/includes/head.blade.php
+++ resources/views/client/includes/head.blade.php
@@ -3,34 +3,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('meta')
 
-    <!-- الأيقونة -->
-    <link rel="icon" type="image/png" href="{{ asset('clientpage/images/MERCI_IMG/logo_header/Logo-Merci-b3.png') }}" />
+    <link rel="icon" type="image/png" href="{{ secure_asset('clientpage/images/MERCI_IMG/logo_header/Logo-Merci-b3.png') }}" />
 
-    <!-- خطوط ومواد خارجية -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" />
+    <!-- Local stylesheets -->
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/fonts/themify/themify-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/animsition/css/animsition.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/daterangepicker/daterangepicker.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/slick/slick.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/vendor/lightbox2/css/lightbox.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/util.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/main.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('clientpage/css/all.min.css') }}">
+
+    <!-- External stylesheets -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.css" integrity="sha256-NAxhqDvtY0l4xn+YVa6WjAcmd94NNfttjNsDmNatFVc=" crossorigin="anonymous" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
 
-    <!-- تحميلات Bootstrap (الإصدار الأحدث فقط) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- jQuery and Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
-    <!-- ملفات CSS محلية -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/css/util.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/css/main.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/css/all.min.css') }}">
-
-    <!-- ملفات CSS أخرى محلية -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/fonts/themify/themify-icons.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/animate/animate.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/select2/select2.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/daterangepicker/daterangepicker.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/slick/slick.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('clientpage/vendor/lightbox2/css/lightbox.min.css') }}">
-
-    <!-- مكتبة الجافاسكريبت -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.6.12/dist/css/splide.min.css">
 </head>
