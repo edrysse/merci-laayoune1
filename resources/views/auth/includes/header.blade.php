14e69395681503d3f38b69b750b9c4f7f4bca7ff
edrysse <117366780+edrysse@users.noreply.github.com>
2024-10-27 16:45:52 +0100

    last

--- resources/views/auth/includes/header.blade.php
+++ resources/views/auth/includes/header.blade.php
@@ -7,15 +7,15 @@
     <link rel="icon" type="image/png" href="{{ asset('loginpage/images/icons/favicon.ico') }}" />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('loginpage/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/vendor/bootstrap/css/bootstrap.min.css') }}">
 
     <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="{{ asset('loginpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
 
     <!-- Additional CSS -->
-    <link rel="stylesheet" href="{{ asset('loginpage/vendor/animate/animate.css') }}">
-    <link rel="stylesheet" href="{{ asset('loginpage/vendor/css-hamburgers/hamburgers.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('loginpage/vendor/select2/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('loginpage/css/util.css') }}">
-    <link rel="stylesheet" href="{{ asset('loginpage/css/main.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/vendor/animate/animate.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/vendor/select2/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/css/util.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('loginpage/css/main.css') }}">
 </head>
